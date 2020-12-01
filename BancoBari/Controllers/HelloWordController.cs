f98e86511297b837ac1f898574747554f097ff2b
Alison Alves <alisonsantos2017@outlook.com>
2020-11-30 22:10:45 -0300

    Criação API Banco Bari

---
+++ BancoBari/Controllers/HelloWordController.cs
@@ -0,0 +1,66 @@
+﻿using BancoBari.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BancoBari.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWordController : ControllerBase
+    {
+        private ILogger<HelloWord> _logger;
+
+        public HelloWordController(ILogger<HelloWord> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GetHelloWord()
+        {
+            try
+            {
+                HelloWord helloWord = new HelloWord(
+                    Guid.NewGuid(), 
+                    "Hello World!", 
+                    DateTime.Now.ToString("yyyyMMddHHmmssffff"));
+                
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var filaMessage = JsonSerializer.Serialize(helloWord);
+                    var body = Encoding.UTF8.GetBytes(filaMessage);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", filaMessage);
+                }
+
+                return Accepted(helloWord);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro ao tentar executar mensagem", ex);
+
+                return new StatusCodeResult(500);
+            }
+            
+        }
+    }
+}
---
+++ BancoBari/Domain/HelloWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoBari.Domain
+{
+    public sealed class HelloWord
+    {
+        public HelloWord(Guid id, string message, string tempo)
+        {
+            Id = id;
+            Message = message;
+            Tempo = tempo;
+        }
+
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public string Tempo { get; set; }
+    }
+}
